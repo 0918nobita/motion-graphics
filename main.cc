dd40d66d5384b9565f9c32e67281b97c9a7c2181
0918nobita <nobita.0918@gmail.com>
2022-05-21 18:01:13 +0900

    :construction: コーデックコンテキストの取得処理を追加した

--- main.cc
+++ main.cc
@@ -1,45 +1,92 @@
 #include <iostream>
+#include <memory>
 
 extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 }
 
-int main(int argc, char *argv[]) {
+class FormatContext {
+  AVFormatContext* format_context;
+
+  FormatContext(AVFormatContext* context) { format_context = context; }
+
+ public:
+  static FormatContext open_input(const char* url) {
+    AVFormatContext* format_context = nullptr;
+    avformat_open_input(&format_context, url, nullptr, nullptr);
+    if (format_context == nullptr) {
+      throw std::runtime_error("Could not open input");
+    } else {
+      return FormatContext(format_context);
+    }
+  }
+
+  AVStream* find_video_stream() {
+    AVStream* video_stream = nullptr;
+
+    for (int i = 0; i < (int)format_context->nb_streams; ++i) {
+      if (format_context->streams[i]->codecpar->codec_type ==
+          AVMEDIA_TYPE_VIDEO) {
+        video_stream = format_context->streams[i];
+        break;
+      }
+    }
+
+    if (video_stream == nullptr) {
+      throw std::runtime_error("No video stream found");
+    } else {
+      return video_stream;
+    }
+  }
+
+  ~FormatContext() {
+    avformat_close_input(&format_context);
+  }
+};
+
+class CodecContext {
+  AVCodecContext* codec_context;
+
+  CodecContext(AVCodecContext* context) { codec_context = context; }
+
+ public:
+  static CodecContext alloc(const AVCodec* codec) {
+    AVCodecContext* codec_context = avcodec_alloc_context3(codec);
+    if (codec_context == nullptr) {
+      throw std::runtime_error("Could not allocate codec context");
+    } else {
+      return CodecContext(codec_context);
+    }
+  }
+
+  ~CodecContext() {
+    avcodec_free_context(&codec_context);
+  }
+};
+
+int main(int argc, char* argv[]) {
   if (argc < 2) {
     std::cerr << "Usage: " << argv[0] << " <file>" << std::endl;
     return EXIT_FAILURE;
   }
 
-  AVFormatContext *format_context = nullptr;
+  try {
+    auto format_context = FormatContext::open_input(argv[1]);
 
-  if (avformat_open_input(&format_context, argv[1], nullptr, nullptr) != 0) {
-    std::cerr << "avformat_open_input failed" << std::endl;
-    return EXIT_FAILURE;
-  }
+    auto video_stream = format_context.find_video_stream();
 
-  AVStream *video_stream = nullptr;
-
-  for (int i = 0; i < (int)format_context->nb_streams; ++i) {
-    if (format_context->streams[i]->codecpar->codec_type ==
-        AVMEDIA_TYPE_VIDEO) {
-      video_stream = format_context->streams[i];
-      break;
+    const AVCodec* codec =
+        avcodec_find_decoder(video_stream->codecpar->codec_id);
+    if (codec == nullptr) {
+      throw std::runtime_error("No supported decoder found");
     }
-  }
 
-  if (video_stream == nullptr) {
-    std::cerr << "No video stream found" << std::endl;
+    auto codec_context = CodecContext::alloc(codec);
+  } catch (std::runtime_error& e) {
+    std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
   }
-
-  const AVCodec *codec = avcodec_find_decoder(video_stream->codecpar->codec_id);
-  if (codec == nullptr) {
-    std::cerr << "No supported decoder found" << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  avformat_close_input(&format_context);
 
   return EXIT_SUCCESS;
 }
